22b5eec595c7531b68a7561e13b661f255fbf280
SAGAR T <ssagu6661@gmail.com>
2024-08-14 10:50:40 -0400

    Add files via upload

---
+++ c8.py
@@ -0,0 +1,16 @@
+
+#BOLLEAN
+
+name2=print(bool(0)) #0 will NOT considered the true
+print(bool())   # empty bracket NOT cosidered true
+print(bool(123456789))  # numbers will  considered the true
+name =  "prjwal gowda s"
+print("prjwal" in name)
+print(" T" in name)
+
+
+
+print("hello sagar")
+print(type(name2))
+print(type(False))
+print(type(True))
